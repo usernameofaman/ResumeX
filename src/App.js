824b0d953d13e9876246e0a62ee53aa2ad3e726b
Aman_AmanSharma <aman@leewayhertz.com>
2021-12-20 12:44:24 +0530

    Added a comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Resume from "./Components/Resume"
 // import Timer from "./Components/TimerHome"
 import "./App.css"
 
+//This is a comment that i am adding
+
 function App() {
     
     return (
